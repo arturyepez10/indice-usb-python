bb5999e3c2e728ba76e76a2beaf37df547f451f2
Arturo Yepez <arturyepez10@gmail.com>
2024-08-01 16:02:48 -0400

    feat: update main to accept custom file names

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
-from models import Course, AcademicPeriod, AcademicSummary
+import argparse
+
+from models import Course
 from file import DataReader
 
-first_period = (
-  "Abril-Julio", 2016,
-  [
-    Course("MA1111", "Matematicas I", 4, 3),
-    Course("CSA211", "Sociales I", 3, 5),
-    Course("LLA111", "Lenguaje I", 3, 3),
-    Course("ID1111", "Ingles I", 3, 5),
-    Course("MA1511", "Curso de Geometria", 2, 1)
-  ]
-)
+def main(parser: argparse.ArgumentParser):
 
-second_period = (
-  "Septiembre-Diciembre", 2016,
-  [
-    Course("MA1112", "Matematicas II", 4, 2),
-    Course("FS1111", "Fisica I", 3, 1),
-    Course("CSA212", "Sociales II", 3, 3),
-    Course("LLA112", "Lenguaje II", 3, 4),
-    Course("ID1112", "Ingles II", 3, 5),
-  ]
-)
+  parameters = parser.parse_args()
 
-def main():
+  file_name = parameters.n
+
+  if not parameters.n:
+    print("[WARNING] custom name not found for the project")
+    file_name = "expediente.csv"
+
   reader = DataReader()
-  reader.load_file("expediente.csv")
+  reader.load_file(file_name)
   reader.read_file()
 
   summary = reader.summary
@@ -40,4 +29,14 @@ def main():
   print("total grade:", summary.summary_grade())
 
 if __name__ == "__main__":
-  main()
+  parse = argparse.ArgumentParser("Indice Academico | USB")
+
+  parse.add_argument(
+    "-name",
+    "--n",
+    metavar="N",
+    help="Nombre del archivo de hoja de cálculo con el expediente",
+    required=False
+  )
+
+  main(parse)
